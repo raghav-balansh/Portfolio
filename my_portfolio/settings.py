43055460f73c0e5d2cf8e158575321569bed0fc0
raghav-balansh <raghav.balansh@gmail.com>
2025-11-11 10:11:55 +0530

    app create

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'project',
+    'content',
+    'blog',
+    'contact',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
